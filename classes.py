7dddef04b6a7c4bf2f58c286b88a376707944ed0
adeilertsen <adrianeilertsen@hotmail.no>
2023-04-17 11:23:10 +0200

    Initial commit

---
+++ classes.py
@@ -0,0 +1,62 @@
+def pdf_to_mp3(text_input, voice_name, language):
+
+    synthesis_input = texttospeech.SynthesisInput(text=text_input)
+    current_voice = texttospeech.VoiceSelectionParams(language_code=language, name=voice_name)
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+
+    # ----------GETTING A SAMPLE OF EVERY VOICE IN EVERY LANGUAGE---------
+    #
+    # languages = ["da-DK", "en-GB", "nb-NO", "sv-SE"]
+    # for lang in languages:
+    #    voices = client.list_voices(language_code="lang")
+    #    for voice in voices.voices:
+    #        current_voice = texttospeech.VoiceSelectionParams(language_code="lang",
+    #                                                              name=voice.name)
+    #        response = client.synthesize_speech(input=synthesis_input, voice=current_voice, audio_config=audio_config)
+    #        with open(f"{voice.name}.mp3", "wb") as out:
+    #            out.write(response.audio_content)
+    #
+
+    response = client.synthesize_speech(input=synthesis_input, voice=current_voice, audio_config=audio_config)
+    with open("static/download/output.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print("Audio content written to file 'output.mp3'")
+    return response
+
+
+def voice_samples(voices_list, lang):
+    voice_sample = []
+    for voice in voices_list:
+        sample_text = ""
+        if lang == "en-GB":
+            sample_text = "Hi, do you want this voice?"
+        elif lang == "no-NO":
+            sample_text = "Hei, vil du ha denne stemmen?"
+        elif lang == "sv-SE":
+            sample_text = "hej, vill du ha den här rösten?"
+        sample = pdf_to_mp3(sample_text, voice.name, lang, 1)
+        voice_sample.append(sample)
+    return voice_sample
+
+
+def get_voices(language):
+    voices = client.list_voices(language_code=language)
+    selected_voices = []
+    num = 0
+    for voice in voices.voices:
+        splitted_voice_name = voice.name.split("-")
+        if num == 4:
+            break
+        elif splitted_voice_name[2] == "Wavenet":
+            selected_voices.append(voice)
+            num += 1
+            print(voice)
+    return selected_voices
+
+def pdf_to_text(filename):
+    with open(filename, 'rb') as f:
+        pdf = PdfReader(f)
+        text = ""
+        for page in pdf.pages:
+            text = text + page.extract_text()
+        return text
---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+from google.cloud import texttospeech
+from flask import Flask, render_template, request, redirect
+from PyPDF2 import PdfReader
+from classes import *
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cohesive-armor-381212-0b44764aae31.json"
+client = texttospeech.TextToSpeechClient()
+
+
+ENGLISH = {
+    "language": "en-GB",
+    "voices": ["en-GB-Wavenet-A", "en-GB-Wavenet-B", "en-GB-Wavenet-C", "en-GB-Wavenet-D"],
+    "samples": ["Language/en-GB-Wavenet-A.mp3", "Language/en-GB-Wavenet-B.mp3",
+                "Language/en-GB-Wavenet-C.mp3", "Language/en-GB-Wavenet-D.mp3"],
+    "gender": ["female", "male", "female", "male"]
+}
+NORWEGIAN = {
+    "language": "nb-NO",
+    "voices": ["nb-NO-Wavenet-A", "nb-NO-Wavenet-B", "nb-NO-Wavenet-C", "nb-NO-Wavenet-D"],
+    "samples": ["Language/nb-NO-Wavenet-A.mp3", "Language/nb-NO-Wavenet-B.mp3",
+                "Language/nb-NO-Wavenet-C.mp3", "Language/nb-NO-Wavenet-D.mp3"],
+    "gender": ["female", "male", "female", "male"]
+}
+SWEDISH = {
+    "language": "sv-SE",
+    "voices": ["sv-SE-Wavenet-A", "sv-SE-Wavenet-B", "sv-SE-Wavenet-C", "sv-SE-Wavenet-D"],
+    "samples": ["Language/sv-SE-Wavenet-A.mp3", "Language/sv-SE-Wavenet-B.mp3",
+                "Language/sv-SE-Wavenet-C.mp3", "Language/sv-SE-Wavenet-D.mp3"],
+    "gender": ["female", "female", "male", "female"]
+}
+
+app = Flask(__name__)
+language = ""
+selected_voice = ""
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/voice/<lang>", methods=["GET", "POST"])
+def voice(lang):
+    global language
+    data = {}
+    if lang == "en-GB":
+        data = ENGLISH
+    elif lang == "nb-NO":
+        data = NORWEGIAN
+    elif lang == "sv-SE":
+        data = SWEDISH
+    language = lang
+    return render_template("voice.html", data=data)
+
+
+@app.route("/input/<voice>", methods=["GET", "POST"])
+def input(voice):
+    global language
+    global selected_voice
+    if selected_voice == "":
+        selected_voice = voice
+
+    if request.method == "POST":
+        file = request.files['file']
+        pdf = PdfReader(file)
+        text = ""
+        for page in pdf.pages:
+            text = text + page.extract_text()
+        print(text)
+        print(language)
+        print(selected_voice)
+        pdf_to_mp3(text, selected_voice, language)
+        return redirect("/download")
+    return render_template("input.html")
+
+
+@app.route("/download")
+def download():
+    return render_template("download.html")
+
+
+if __name__ == "__main__":
+    app.run()
